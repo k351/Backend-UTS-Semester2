3e1c134a4c5546941390d71814e2820de101670e
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2024-05-05 22:22:38 +0700

    Kembali jadi commit sebelumnya

--- src/api/components/transactions/transactions-service.js
+++ src/api/components/transactions/transactions-service.js
@@ -76,8 +76,6 @@ async function getTransactionbyId(id, user_id) {
  * @returns {boolean}
  */
 async function updateTransaction(id, receiver_id, amount) {
-  const receiver =  await getUser(receiver_id);
-
   const transaction = await transactionsRepository.getTransactionbyId(id);
 
   // User not found
@@ -86,11 +84,6 @@ async function updateTransaction(id, receiver_id, amount) {
   }
 
   try {
-    sender.balance -= (amount - transaction.amount);
-    receiver.balance += (amount - transaction.amount);
-
-    // Save changes to the database
-    await Promise.all([sender.save(), receiver.save()]);
     await transactionsRepository.updateTransaction(id, receiver_id, amount);
   } catch (err) {
     return null;
